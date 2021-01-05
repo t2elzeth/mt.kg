96bbf426913aaa5f567b9e0b6c6e62a702c958f2
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-01-05 20:18:27 +0600

    update docker-compose

--- production/gunicorn/gunicorn_config.py
+++ production/gunicorn/gunicorn_config.py
@@ -1,8 +1,8 @@
 pythonpath = '/app'
-bind = '0.0.0.0:8000'
+bind = '194.58.103.226:8000'
 workers = 5
 timeout = 6000
-user = 'root'
+user = 'ulukman'
 limit_request_fields = 32000
 limit_request_field_size = 0
 raw_env = 'DJANGO_SETTINGS_MODULE=config.prod.settings'
