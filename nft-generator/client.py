534d88f11282c4614f5869f5bdee2532ab69335f
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-01-07 19:52:47 +0600

    Commit

--- nft-generator/client.py
+++ nft-generator/client.py
@@ -1,34 +1,32 @@
 import os
 import socketio
-import asyncio
 from pathlib import Path
 
-io = socketio.AsyncClient()
+io = socketio.Client()
 
 BASE_DIR = Path(__file__).resolve().parent
 IMAGES_DIR = 'img'
 tasks = []
 
 
-async def send_imagepath(client: socketio.AsyncClient):
+def send_imagepath(client: socketio.AsyncClient):
     image = input("Enter the imagepath: ")
-    await client.emit("render", os.path.join(IMAGES_DIR, image))
+    client.emit("render", os.path.join(IMAGES_DIR, image))
 
 
 @io.event
-async def connect():
+def connect():
     print('Successfully connected!')
-    await io.emit("render", os.path.join(IMAGES_DIR, 'som200.jpg'))
-    await io.emit("render", os.path.join(IMAGES_DIR, 'me.jpg'))
+    io.emit("render", os.path.join(IMAGES_DIR, 'som200.jpg'))
 
 
 @io.event
-async def success(message):
+def success(message):
     print(message)
 
 
 @io.event
-async def error(message):
+def error(message):
     print(message)
 
 
@@ -38,22 +36,15 @@ def finished(message):
 
 
 @io.event
-async def welcome(message):
+def welcome(message):
     print(message)
-    await io.emit("welcome-back", "Yes, than you!")
+    io.emit("welcome-back", "Yes, than you!")
 
 
 @io.event
-async def disconnect():
+def disconnect():
     print('Disconnected from server')
 
 
-async def run():
-    await io.connect("http://localhost:8920")
-
-    tasks.append(await io.wait())
-    await asyncio.gather(*tasks)
-
-
-if __name__ == '__main__':
-    asyncio.run(run())
+io.connect("http://localhost:8920")
+io.wait()
