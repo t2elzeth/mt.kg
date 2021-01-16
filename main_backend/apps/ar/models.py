fb8b511f0d3060b0000c04c9a54dfce40ea82b0c
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-01-16 14:01:58 +0600

    Preparing for API

--- main_backend/apps/ar/models.py
+++ main_backend/apps/ar/models.py
@@ -2,12 +2,12 @@ from django.db import models
 import os
 
 
-# Create your models here.
 class AR(models.Model):
     """Model for each AR project with its own photo and video"""
     title = models.CharField(max_length=255)
     img = models.ImageField(upload_to='images/', blank=True, null=True)
     video = models.FileField(upload_to='videos/', blank=True, null=True)
+    is_rendered = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
