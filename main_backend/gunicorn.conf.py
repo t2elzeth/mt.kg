ea87b377c29a15d6738eec4a6aad9ac2704d0b9c
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-03-15 21:52:52 +0600

    Update

--- main_backend/gunicorn.conf.py
+++ main_backend/gunicorn.conf.py
@@ -1,6 +1,8 @@
+import multiprocessing
+
 pythonpath = '/app'
 bind = '0.0.0.0:8000'
-workers = 5
+workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 6000
 user = 'root'
 limit_request_fields = 32000
